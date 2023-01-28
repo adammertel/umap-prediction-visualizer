ac235af689164baaf2796aedf9bade4cdfe1d9ec
adammertel <mertel.adam@gmail.com>
2023-01-28 11:39:44 +0100

    Clean repository and install dependencies

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
